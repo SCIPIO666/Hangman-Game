8781771cf483bb0e7a9875171dcfc639f2d5349b
SCIPO666 <eaphoney@gmail.com>
2025-12-03 10:26:02 +0300

    hangman class debugging

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
-const svgParent=document.querySelector("svg"); // Assumes an <svg> element exists in the HTML
+const svgParent=document.querySelector("#hangman"); // Assumes an <svg id="hangman"> element exists
 const svgNamespace = "http://www.w3.org/2000/svg";
-
- 
 
 class HangmanFigure{
     constructor(parentElem, nameSpace){
         this.parentElem = parentElem;
         this.nameSpace = nameSpace;
     }
+    
     drawLine(x1, y1, x2, y2) {
-            // Coordinates <line x1="140" y1="80" x2="140" y2="100"/>
         let line = document.createElementNS(this.nameSpace, "line");
         const attributes = {
-            x1: x1,
+            // FIX: Must use standard SVG line attributes: x1, y1, x2, y2
+            x1: x1, 
             y1: y1,
             x2: x2,
             y2: y2,
         }
-        this.attributeFunction(line, attributes);
+        this.setAttributes(line, attributes);
         this.parentElem.appendChild(line);
     }
         
     setAttributes(elem, object){
-            for (key in object){
-                elem.setAttribute(key, object[key]);
-            }
-            elem.setAttribute("stroke-width", "2");
-            elem.setAttribute("stroke", "black");
-        
+        for (key in object){
+            elem.setAttribute(key, object[key]);
+        }
+        // FIX: Set style attributes outside the loop
+        elem.setAttribute("stroke-width", "2");
+        elem.setAttribute("stroke", "red");
     }
 
-        
     drawNeck(){
-        let line = document.createElementNS(this.nameSpace, "line");
-        const attributes = {
-            x1: 140,
-            y1: 80,
-            x2: 140,
-            y2: 100,
-        }
-        this.attributeFunction(line, attributes);
-        this.parentElem.appendChild(line);
+        this.drawLine(140, 80, 140, 100);
     }
     
-    // Coordinates based on <circle cx="140" cy="60" r="20"/> (Fixed y-coordinate from 600 to 60)
     drawHead(){
         let circle = document.createElementNS(this.nameSpace, "circle");
         const attributes = {
@@ -52,38 +41,29 @@ class HangmanFigure{
             r: 20,
             fill: "white",
         }
-        this.setAttributes(circle,attributes);
+        this.setAttributes(circle, attributes);
         this.parentElem.appendChild(circle);
     }
 
     drawTorso(){
-        // Top line
         this.drawLine(120, 100, 160, 100);
-        // Left vertical line
         this.drawLine(120, 100, 120, 150);
-        // Right vertical line
         this.drawLine(160, 100, 160, 150);
-        // Bottom line
         this.drawLine(120, 150, 160, 150);
     }
     
     drawRightHand(){
-        // Main Arm Line
         this.drawLine(160, 100, 180, 130);
-        // Hand/Fingers
         this.drawLine(160, 110, 170, 120);
         this.drawLine(180, 130, 170, 120);
     }
     
     drawLeftHand(){
-        // Main Arm Line
         this.drawLine(120, 100, 100, 130);
-        // Hand/Fingers
         this.drawLine(120, 110, 110, 120);
         this.drawLine(100, 130, 110, 120);
     }
     
-
     drawRightLeg(){
         this.drawLine(150, 150, 150, 220);
     } 
@@ -92,5 +72,13 @@ class HangmanFigure{
         this.drawLine(130, 150, 130, 220);
     }
 }
-let hangman=new HangmanFigure(svgParent,svgNamespace);
-hangman. drawTorso();
+
+const figure = new HangmanFigure(svgParent, svgNamespace);
+
+figure.drawHead();
+figure.drawNeck();
+figure.drawTorso();
+figure.drawRightHand();
+figure.drawLeftHand();
+figure.drawRightLeg();
+figure.drawLeftLeg();
